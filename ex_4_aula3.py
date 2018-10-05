973a194f61b65ff32183ce0d25c4b23441e67e7f
Gustavobflh <43830003+Gustavobflh@users.noreply.github.com>
2018-10-05 15:19:14 -0300

    Add files via upload

---
+++ ex_4_aula3.py
@@ -0,0 +1,24 @@
+from math import *
+
+#Exercício de conversão de unidades para resolver o exercício da corrida
+
+def milha_metro(milhas):
+
+	m = milhas/1609.34
+	return(m)
+
+def metro_milha(m)
+
+	milhas=m*1609.34
+	return(milhas)
+
+def horas_seg(h)
+
+	seg= 60*60*h
+	return 
+
+def minutos_hora(minute)
+
+	h= minute/60 
+	return(h) 
+
